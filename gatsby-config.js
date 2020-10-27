b6cb330e695720b694a5540b22b5972381159560
Jarrod Ng <jarrodngjg@gmail.com>
2020-10-27 11:55:09 +0800

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require(`dotenv`).config({
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
-  pathPrefix: "/jarrod-portfolio",
+  pathPrefix: "/jarrod-ng.github.io",
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
